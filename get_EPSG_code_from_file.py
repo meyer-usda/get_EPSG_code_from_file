faf74563509299562bbff46455f52ec9f6addf49
Meyer Taffel <meyer.taffel@usda.gov>
2025-02-06 11:47:50 -0700

    Update code to gracefully handle raster or pointcloud

--- get_EPSG_code_from_file.py
+++ get_EPSG_code_from_file.py
@@ -1,7 +1,7 @@
 import rasterio, laspy, argparse, os
 
-parser = argparse.ArgumentParser(description='Provide a las file and get the EPSG code from the header')
-parser.add_argument('-f', '--file', type=str, help=r'full\path\to\las\file')
+parser = argparse.ArgumentParser(description='Provide a raster or las/laz file and get the EPSG code from the EXIF/header')
+parser.add_argument('-f', '--file', type=str, help=r'full\path\to\target\file')
 args = parser.parse_args()
 
 if args.file:
@@ -13,10 +13,14 @@ else:
 filetype = os.path.splitext(filepath)[1]
 
 if filetype in [".las", '.laz']:
-    las = laspy.read(filepath)
-    epsg = las.header.parse_crs()
+    try:
+        las = laspy.read(filepath)
+        print(las.header.parse_crs())
+    except:
+        print("Couldn't find the EPSG code in your file header.")
 else:
-    with rasterio.open(filepath) as src:
-        epsg = src.crs
-
-print(epsg)
+    try:
+        with rasterio.open(filepath) as src:
+            print(src.crs)
+    except:
+        print("Couldn't find EPSG code. Check if your file type is a raster format (.tif, etc)")
